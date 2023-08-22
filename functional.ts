54e5f2d206f25a9e58d09de497698ca835465ff9
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-08-22 19:56:44 +0900

    map, filter, reduce Array 객체 지원

--- functional.ts
+++ functional.ts
@@ -22,26 +22,63 @@ export function fold<T, R>(
   return fold(iter, callback, callback(initial, value), index + 1);
 }
 
-export function* filter<T>(
-  iter: Iterable<T>,
-  callback: (curr: T, index?: number) => boolean,
   index: number = 0
+
+export function filter<T>(
+  callback: (curr: T) => boolean,
+  iterable: Iterable<T>
+) {
+  return Array.isArray(iterable)
+    ? filterIter(callback, iter(iterable))
+    : filterIter(callback, iterable);
+}
+function* filterIter<T>(
+  callback: (curr: T) => boolean,
+  iterable: Iterable<T>
 ): Iterable<T> {
-  const { value, done } = iter[Symbol.iterator]().next();
+  const { value, done } = iterable[Symbol.iterator]().next();
   if (done) return;
-  if (callback(value, index)) yield value;
-  yield* filter(iter, callback, index + 1);
+  if (callback(value)) yield value;
+  yield* filterIter(callback, iterable);
 }
 
-export function* map<T, R>(
-  iter: Iterable<T>,
-  callback: (curr: T, index?: number) => R,
-  index: number = 0
+export function map<T, R>(
+  callback: (curr: T) => R,
+  iterable: Iterable<T> | T[]
+) {
+  return Array.isArray(iterable)
+    ? mapIter(callback, iter(iterable))
+    : mapIter(callback, iterable);
+}
+
+function* mapIter<T, R>(
+  callback: (curr: T) => R,
+  iter: Iterable<T>
 ): Iterable<R> {
   const { value, done } = iter[Symbol.iterator]().next();
   if (done) return;
-  yield callback(value, index);
-  yield* map(iter, callback, index + 1);
+  yield callback(value);
+  yield* mapIter(callback, iter);
+}
+
+export function reduce<T, R>(
+  callback: (acc: R, curr: T, index?: number) => R,
+  acc: R,
+  iterable: Iterable<T> | T[]
+) {
+  return Array.isArray(iterable)
+    ? reduceIter(callback, acc, iter(iterable))
+    : reduceIter(callback, acc, iterable);
+}
+
+function reduceIter<T, R>(
+  callback: (acc: R, curr: T) => R,
+  acc: R,
+  iterable: Iterable<T>
+): R {
+  const { value, done } = iterable[Symbol.iterator]().next();
+  if (done) return acc;
+  return reduceIter(callback, callback(acc, value), iterable);
 }
 
 function oneOrZero(n: number) {
@@ -77,4 +114,5 @@ export const pipe =
   (x: T) =>
     fns.reduce((v, f) => f(v), x) as unknown as R;
 
-export const iter = <T>(arr: Array<T>) => arr[Symbol.iterator]();
+export const iter: <T>(arr: T[]) => Iterable<T> = (arr) =>
+  arr[Symbol.iterator]();
